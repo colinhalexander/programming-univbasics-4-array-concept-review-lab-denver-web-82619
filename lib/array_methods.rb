c728dbff0c10956939f067762d533026d429c7b2
Colin Alexander <chalexander@me.com>
2019-08-18 17:34:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,24 +14,28 @@ end
 def find_max_value(array)
   count = 0
   max = 0
+  
   while array[count] do
     if array[count] > max
       max = array[count]
     end
     count += 1
-  end  
+  end
+  
   return max
 end
 
 def find_min_value(array)
   count = 0
   min = array[0]
+  
   while array[count] do
     if array[count] < min
       min = array[count]
     end
     count += 1
   end  
+  
   return min
 end
 
